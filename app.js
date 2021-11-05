13ff0257d5976f3a72ff80fd9df86deb728c911e
sefikkesim <sefikkesim@gmail.com>
2021-11-05 11:47:52 +0200

    Navbar

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ let btn = document.querySelector(".toggle-btn")
 
 
 
+// btn.addEventListener("click",()=>{
+//     if(links.classList.contains("show-links")){
+//         links.classList.remove("show-links")
+//     }else{
+//         links.classList.add("show-links")
+//     }
+// })
+
+
 btn.addEventListener("click",()=>{
-    if(links.classList.contains("show-links")){
-        links.classList.remove("show-links")
-    }else{
-        links.classList.add("show-links")
-    }
+    links.classList.toggle("show-links")
 })
